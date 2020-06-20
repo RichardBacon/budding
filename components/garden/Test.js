1435a0f98049e3797d586dc124e4784e57caa712
ZachPinfold <Zach@tibbi.co.uk>
2020-06-20 20:57:07 +0100

    setup the moving marker

--- components/garden/Test.js
+++ components/garden/Test.js
@@ -1,14 +1,26 @@
-import React, {Component} from 'react';
-import {View, Animated, StyleSheet, Easing, Text} from 'react-native';
-import Arrow from '../../assets/tutorials/part_2/arrow.svg'
+import React, { Component } from 'react';
+import {
+  View,
+  Animated,
+  StyleSheet,
+  Easing,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import Arrow from '../../assets/tutorials/part_2/arrow.svg';
+import ArrowFlipped from '../../assets/tutorials/part_2/arrow_flipped.svg';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class Loop extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      startValue: new Animated.Value(1),
+      startValue: new Animated.Value(50),
       endValue: 2,
       duration: 1000,
+      button1: new Animated.Value(1),
+      button2: new Animated.Value(1),
+      button3: new Animated.Value(1),
     };
   }
 
@@ -17,18 +29,44 @@ export default class Loop extends Component {
       Animated.sequence([
         Animated.timing(this.state.startValue, {
           toValue: 50,
-          duration: 500,
-          // delay: 1000
+          duration: 1000,
         }),
         Animated.timing(this.state.startValue, {
+          toValue: 25,
+          duration: 1000,
+        }),
+        Animated.timing(this.state.button1, {
+          toValue: 0,
+          duration: 1,
+        }),
+        Animated.timing(this.state.startValue, {
+          delay: 1000,
           toValue: 1,
-          duration: 500
-        })
+          duration: 1000,
+        }),
+        Animated.timing(this.state.button2, {
+          toValue: 0,
+          duration: 1,
+        }),
+        Animated.timing(this.state.startValue, {
+          delay: 1000,
+          toValue: 50,
+          duration: 1000,
+          opacity: 0.1,
+        }),
+        Animated.timing(this.state.button1, {
+          toValue: 1,
+          duration: 1,
+        }),
+        Animated.timing(this.state.button2, {
+          toValue: 1,
+          duration: 1,
+        }),
       ]),
       {
         // iterations: 4
-      }
-    ).start()
+      },
+    ).start();
     // Animated.loop(
     //   Animated.spring(this.state.startValue, {
     //     toValue: this.state.endValue,
@@ -48,6 +86,7 @@ export default class Loop extends Component {
           style={[
             styles.square,
             {
+              // opacity: this.state.fadeAnim,
               transform: [
                 {
                   translateY: this.state.startValue,
@@ -55,28 +94,142 @@ export default class Loop extends Component {
               ],
             },
           ]}
-        ><Arrow height={30}></Arrow></Animated.View>
+        >
+          <View style={styles.arrow_container}>
+            <View style={{ marginRight: 15 }}>
+              <ArrowFlipped height={30} width={30}></ArrowFlipped>
+            </View>
+            <View>
+              <View style={styles.oval}></View>
+              <View style={styles.horizontal_line} />
+              <View style={styles.vertical_line} />
+            </View>
+            <View style={{ marginLeft: 15 }}>
+              <Arrow height={30} width={30}></Arrow>
+            </View>
+          </View>
+          <View style={{ marginLeft: 145 }}>
+            <Animated.View
+              style={[
+                styles.button_next_2,
+                {
+                  opacity: this.state.button3,
+                },
+              ]}
+            >
+              <TouchableOpacity style={[styles.button_next_2]}>
+                <Text style={styles.button_text_step_2}>third marker</Text>
+              </TouchableOpacity>
+            </Animated.View>
+            <Animated.View
+              style={[
+                styles.button_next_2,
+                {
+                  opacity: this.state.button2,
+                },
+              ]}
+            >
+              <TouchableOpacity style={[styles.button_next_2]}>
+                <Text style={styles.button_text_step_2}>second marker</Text>
+              </TouchableOpacity>
+            </Animated.View>
+            <Animated.View
+              style={[
+                styles.button_next,
+                {
+                  opacity: this.state.button1,
+                },
+              ]}
+            >
+              <TouchableOpacity style={[styles.button_next]}>
+                <Text style={styles.button_text_step_2}>first marker</Text>
+              </TouchableOpacity>
+            </Animated.View>
+          </View>
+        </Animated.View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  // },
+  container: {
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // alignContent: 'center',
+  },
+  arrow_container: {
+    flexDirection: 'row',
+    alignItems: 'center', // width: 1,
+    // justifyContent: 'center',
+    alignItems: 'center',
+    // alignContent: 'center',
+    // width: 100,
+  },
   square: {
     height: 20,
     width: 50,
     // backgroundColor: 'red',
   },
+  oval: {
+    zIndex: 10,
+    elevation: 11,
+    position: 'relative',
+    height: 70,
+    width: 200,
+    opacity: 0.3,
+    backgroundColor: '#fdbe39',
+    borderRadius: 200,
+  },
+  horizontal_line: {
+    position: 'absolute',
+    height: 1,
+    width: 200,
+    marginTop: 35,
+    opacity: 1,
+    backgroundColor: 'red',
+    borderRadius: 5,
+  },
+  vertical_line: {
+    position: 'absolute',
+    height: 70,
+    width: 1,
+    marginLeft: 100,
+    opacity: 1,
+    backgroundColor: 'red',
+    borderRadius: 5,
+  },
+  button_next: {
+    position: 'absolute',
+    backgroundColor: '#52875a',
+    borderRadius: 5,
+    // marginLeft: 240,
+    marginTop: 20,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: 200,
+    height: 45,
+  },
+  button_next_2: {
+    position: 'absolute',
+    backgroundColor: '#52875a',
+    borderRadius: 5,
+    // marginLeft: 240,
+    marginTop: 20,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: 200,
+    height: 45,
+  },
+  button_text_step_2: {
+    fontSize: 25,
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
 });
-        
-        
-        
-        
-        // style={{
-        //   transform: [{ translateX: 1 }, { translateY: 100 }]
-        // }}
+
+// style={{
+//   transform: [{ translateX: 1 }, { translateY: 100 }]
+// }}
---
+++ utils/animations/MeasureAnimation.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { View, Animated, StyleSheet, Easing, Text } from 'react-native';
+import Arrow from '../../assets/tutorials/part_2/arrow.svg';
+
+export default class ArrowHead extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startValue: new Animated.Value(1),
+      endValue: 2,
+      duration: 1000,
+    };
+  }
+
+  componentDidMount() {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(this.state.startValue, {
+          toValue: 80,
+          duration: 800,
+          // delay: 1000
+        }),
+        Animated.timing(this.state.startValue, {
+          toValue: 1,
+          duration: 500,
+        }),
+      ]),
+      {
+        // iterations: 4
+      },
+    ).start();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Animated.View
+          style={[
+            styles.square,
+            {
+              transform: [
+                {
+                  translateY: this.state.startValue,
+                },
+              ],
+            },
+          ]}
+        >
+          <Arrow height={30}></Arrow>
+        </Animated.View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // },
+  square: {
+    height: 20,
+    width: 50,
+    // backgroundColor: 'red',
+  },
+});
+
+// style={{
+//   transform: [{ translateX: 1 }, { translateY: 100 }]
+// }}
